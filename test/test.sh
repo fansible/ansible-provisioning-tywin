d07e6e1fb9b78ebe99fd4e9c9047c4c774e3a632
Maxime Thoonsen <maxime.thoonsen@gmail.com>
2015-07-04 17:38:04 +0200

    add some tests

--- test/test.sh
+++ test/test.sh
@@ -1,1 +1,3 @@
-python ../generate.py
+cd no-generation/badcomposer && python ../../../generate.py && cd -
+cd no-generation/no-composer-no-package && python ../../../generate.py && cd -
+cd symfony/complete && python ../../../generate.py && rm -rf devops && rm ansible.cfg && rm Vagrantfile  && cd -
