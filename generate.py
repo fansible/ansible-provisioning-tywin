9ab376269e1e36cebd00a8ba6edfeea361960933
Maxime Thoonsen <maxime.thoonsen@gmail.com>
2015-07-04 11:45:32 +0200

    copy only the role needed. Refacto

--- generate.py
+++ generate.py
@@ -1,16 +1,26 @@
 import yaml, os, shutil, sys
 from jinja2 import Environment, FileSystemLoader
 
+#Directories
 DIRECTORY_DEVOPS = 'devops'
 DIRECTORY_PROVISIONING = DIRECTORY_DEVOPS+'/provisioning'
 DIRECTORY_PROVISIONING_ROLES = DIRECTORY_PROVISIONING+'/roles'
 DIRECTORY_PROVISIONING_VARS = DIRECTORY_PROVISIONING+'/vars'
 DIRECTORY_PROVISIONING_HOSTS = DIRECTORY_PROVISIONING+'/hosts'
+DIRECTORY_PROVISIONING_HOSTS_GROUP_VARS = DIRECTORY_PROVISIONING+'/group_vars'
 DIRECTORY_PROVISIONING_FANSIBLE_ROLES = DIRECTORY_PROVISIONING_ROLES+'/fansible-roles'
 DIRECTORY_TYWIN = os.path.dirname(os.path.abspath(__file__))
 DIRECTORY_TYWIN_ROLES = DIRECTORY_TYWIN+'/roles'
 DIRECTORY_TYWIN_TEMPLATES = DIRECTORY_TYWIN+'/templates'
+DIRECTORY_TYWIN_TEMPLATES_ANSIBLE = DIRECTORY_TYWIN_TEMPLATES+'/Ansible'
+DIRECTORY_TYWIN_TEMPLATES_ANSIBLE_VARS = DIRECTORY_TYWIN_TEMPLATES_ANSIBLE+'/Vars'
 
+#Files
+FANSIBLE_YAML = '.fansible.yml'
+DEFAULT_FANSIBLE_YAML = DIRECTORY_TYWIN+'/default.fansible.yml'
+KNOWN_SERVICES = DIRECTORY_TYWIN+'/known_services.yml'
+
+#Configs
 TEMPLATE_ENVIRONMENT = Environment(
     autoescape=False,
     loader=FileSystemLoader(DIRECTORY_TYWIN_TEMPLATES),
@@ -18,34 +28,21 @@ TEMPLATE_ENVIRONMENT = Environment(
 )
 
 def main(argv):
-    project_type_finder()
-    context = build_context()
+    #Build config reading .fansible.yml file
+    project_config = build_config()
+    #Useless for the moment
+    project_type_finder(project_config)
+    #Create all the needed directories
     create_directories()
-    copy_vars_files(context)
+    #Copy all the roles files
+    copy_roles_files(project_config)
+    #generate playbook, ansible.cfg, Vagrantfile, ..
+    generate_basic_provisioning_files_if_they_dont_exist(project_config)
 
-    #Copy roles if not exists
-    if not os.path.exists(DIRECTORY_PROVISIONING_FANSIBLE_ROLES):
-        shutil.copytree(DIRECTORY_TYWIN_ROLES, DIRECTORY_PROVISIONING_FANSIBLE_ROLES)
-    #Copy ansible.cfg
-    if not os.path.exists('ansible.cfg'):
-        shutil.copy(DIRECTORY_TYWIN_TEMPLATES+'/Ansible/ansible.cfg', 'ansible.cfg')
-    #generate playbooks if not exists
-    if not os.path.exists(DIRECTORY_PROVISIONING+'/playbook.yml'):
-        generate_template_file('Ansible/playbook.yml', context, DIRECTORY_PROVISIONING+'/playbook.yml')
-    #generate vagrantfile if not exists
-    if not os.path.exists('Vagrantfile'):
-        generate_template_file('Vagrant/Vagrantfile', context, 'Vagrantfile')
-    #generate vagrant inventory if not exists
-    if not os.path.exists(DIRECTORY_PROVISIONING_HOSTS+'/vagrant'):
-        generate_template_file('Vagrant/vagrant', context, DIRECTORY_PROVISIONING_HOSTS+'/vagrant')
-    #generate vagrant group_vars if not exists
-    if not os.path.exists(DIRECTORY_PROVISIONING_HOSTS+'/group_vars/vagrant'):
-        generate_template_file('Vagrant/group_vars', context, DIRECTORY_PROVISIONING_HOSTS+'/group_vars/vagrant')
-
-    print "Provisioning generated"
+    print "The provisioning has been generated. Check out the `devops` directory"
 
 #TODO:add more project types
-def project_type_finder():
+def project_type_finder(project_config):
     if os.path.exists('composer.json'):
         if 'symfony/symfony' in open('composer.json').read():
             print 'Symfony project detected...'
@@ -61,37 +58,46 @@ def create_directories():
         os.makedirs(DIRECTORY_PROVISIONING)
     if not os.path.exists(DIRECTORY_PROVISIONING_ROLES):
         os.makedirs(DIRECTORY_PROVISIONING_ROLES)
+    if not os.path.exists(DIRECTORY_PROVISIONING_FANSIBLE_ROLES):
+        os.makedirs(DIRECTORY_PROVISIONING_FANSIBLE_ROLES)
     if not os.path.exists(DIRECTORY_PROVISIONING_VARS):
         os.makedirs(DIRECTORY_PROVISIONING_VARS)
     if not os.path.exists(DIRECTORY_PROVISIONING_HOSTS):
         os.makedirs(DIRECTORY_PROVISIONING_HOSTS)
-    if not os.path.exists(DIRECTORY_PROVISIONING_HOSTS+'/group_vars'):
-        os.makedirs(DIRECTORY_PROVISIONING_HOSTS+'/group_vars')
+    if not os.path.exists(DIRECTORY_PROVISIONING_HOSTS_GROUP_VARS):
+        os.makedirs(DIRECTORY_PROVISIONING_HOSTS_GROUP_VARS)
 
 # Build the tree of vars that will be used to generate template files
-def build_context():
-    # Get the project config
-    f = open('.fansible.yml')
-    config_fansible = yaml.safe_load(f)
-    f.close()
+def build_config():
 
-    # Get the default config
-    fv = open(DIRECTORY_TYWIN+'/default.fansible.yml')
-    config_default = yaml.safe_load(fv)
-    fv.close()
+    #TODO: create a serie of question to generate a custom fansible yaml file
+    config_fansible = read_yaml_file_and_return_dict(FANSIBLE_YAML)
+    config_default = read_yaml_file_and_return_dict(DEFAULT_FANSIBLE_YAML)
+    #TODO:change that for a more dynamic way (reading the roles directory)
+    known_services = read_yaml_file_and_return_dict(KNOWN_SERVICES)
 
     # Overide the default config with the project config
     overide_dict(config_default, config_fansible)
+    # Just to be clear:
+    project_config = config_default
 
-    config_default["selected_services"] = []
+    project_config["selected_services"] = []
+    project_config["vars_files"] = []
+
+    project_config["known_services"] = known_services
     # Calculate selected known_services
-    for key, service in enumerate(config_default['services']):
-        if service in config_default['known_services']:
-            config_default['selected_services'].append(service)
+    for key, service in enumerate(project_config['services']):
+        if service in project_config["known_services"]:
+            project_config['selected_services'].append(service)
+            print "The service " + service + " has been added to the provisioning"
+            service_name = project_config["known_services"][service]['role']
+            # We build the list of vars files that will have to be copied
+            if os.path.exists(DIRECTORY_TYWIN_TEMPLATES_ANSIBLE_VARS+'/'+service_name+'.yml'):
+                project_config["vars_files"].append(service_name)
         else:
             print "Sorry the service "+service+" is unknow by Fansible/Tywin yet"
 
-    return config_default
+    return project_config
 
 # Overide values in a base dict with values from another dict
 def overide_dict(base, new):
@@ -111,14 +117,58 @@ def generate_template_file(template_filename, context, dest):
         file_rendered = render_template(template_filename, context)
         ft.write(file_rendered)
 
-#TODO: render only the file that are not empty
-def copy_vars_files(context):
-    for key, service in enumerate(context['selected_services']):
-        generate_template_file(
-            '/Ansible/Vars/'+service+'.yml',
-            context,
-            DIRECTORY_PROVISIONING_VARS+'/'+service+'.yml'
+def copy_roles_files(project_config):
+    for key, service in enumerate(project_config['selected_services']):
+        service_name = project_config['known_services'][service]['role']
+
+        if service_name in project_config['vars_files']:
+            generate_template_file(
+                '/Ansible/Vars/'+service_name+'.yml',
+                project_config,
+                DIRECTORY_PROVISIONING_VARS+'/'+service_name+'.yml'
+            )
+
+        #We override if the role already exists
+        if os.path.exists(DIRECTORY_PROVISIONING_FANSIBLE_ROLES+'/'+service_name):
+            shutil.rmtree(DIRECTORY_PROVISIONING_FANSIBLE_ROLES+'/'+service_name)
+        shutil.copytree(
+            DIRECTORY_TYWIN_ROLES+'/'+service_name,
+            DIRECTORY_PROVISIONING_FANSIBLE_ROLES+'/'+service_name
         )
+
+def read_yaml_file_and_return_dict(name):
+    if os.path.exists(name):
+        fv = open(name)
+        returned_dict = yaml.safe_load(fv)
+        fv.close()
+    else:
+        returned_dict = {}
+
+    return returned_dict
+
+def generate_basic_provisioning_files_if_they_dont_exist(project_config):
+    #Copy ansible.cfg
+    if not os.path.exists('ansible.cfg'):
+        shutil.copy(DIRECTORY_TYWIN_TEMPLATES+'/Ansible/ansible.cfg', 'ansible.cfg')
+    #generate playbooks if not exists
+    if not os.path.exists(DIRECTORY_PROVISIONING+'/playbook.yml'):
+        generate_template_file('Ansible/playbook.yml', project_config, DIRECTORY_PROVISIONING+'/playbook.yml')
+    # We don't override main vars file
+    if not os.path.exists(DIRECTORY_PROVISIONING_VARS+'/main.yml'):
+        generate_template_file(
+            '/Ansible/Vars/main.yml',
+            project_config,
+            DIRECTORY_PROVISIONING_VARS+'/main.yml'
+        )
+    #generate vagrantfile if not exists
+    if not os.path.exists('Vagrantfile'):
+        generate_template_file('Vagrant/Vagrantfile', project_config, 'Vagrantfile')
+    #generate vagrant inventory if not exists
+    if not os.path.exists(DIRECTORY_PROVISIONING_HOSTS+'/vagrant'):
+        generate_template_file('Vagrant/vagrant', project_config, DIRECTORY_PROVISIONING_HOSTS+'/vagrant')
+    #generate vagrant group_vars if not exists
+    if not os.path.exists(DIRECTORY_PROVISIONING_HOSTS_GROUP_VARS+'/vagrant'):
+        generate_template_file('Vagrant/group_vars', project_config, DIRECTORY_PROVISIONING_HOSTS_GROUP_VARS+'/vagrant')
 
 #TODO: create verbose options
 if __name__ == "__main__":
