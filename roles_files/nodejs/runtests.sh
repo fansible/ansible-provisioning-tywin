e791a2856d763befe34208ae02551287ca50888c
Maxime Thoonsen <maximet@theodo.fr>
2016-12-21 07:59:18 +0100

    update nodejs

--- roles_files/nodejs/runtests.sh
+++ roles_files/nodejs/runtests.sh
@@ -1,26 +1,66 @@
-#!/bin/sh
+#!/bin/bash
+
+IMAGES=(
+    "horneds/stouts-centos7"
+    "horneds/stouts-ubuntu14.04"
+    "horneds/stouts-debian8"
+)
+
+TESTS=(
+    "ansible-playbook -c local --syntax-check test.yml"
+    "ansible-playbook -c local test.yml"
+    "which node"
+    "which npm"
+)
 
 APPDIR=/var/tests
 CURDIR=`pwd`
+ARGS=("$@")
 
-RUNNER=`docker run -v $CURDIR:$APPDIR -w $APPDIR -dit ansible/ubuntu14.04-ansible bash`
+docker info || exit 1
 
 assert () {
-    docker exec -it $RUNNER $1 || ( echo ${2-'Test is failed'} && exit 1 )
+    echo "ASSERT: $1"
+    execute "$@" || ( echo ${2-'Test is failed'} && exit 1 )
+    echo "SUCCESS"
 }
 
-{
-    assert "ansible-playbook -c local --syntax-check test-src.yml"   &&
-    assert "ansible-playbook -c local test-src.yml"                  &&
-    assert "ansible-playbook -c local test-src.yml" | grep changed=0 &&
-
-    assert "which node"
-    assert "which npm"
-
-} || {
-
-    echo "Tests are failed"
-
+execute () {
+    docker exec -it $RUNNER $1
+    return $?
 }
 
-docker stop $RUNNER
+suite () {
+
+    echo "================="
+    echo "RUN IMAGE: $1"
+    echo "================="
+
+    for TEST in "${TESTS[@]}"; do
+        assert "$TEST" || ( echo "FAILED" && exit 1 )
+    done
+
+    echo
+    
+}
+
+
+for IMAGE in "${IMAGES[@]}"
+do
+
+    RUNNER=`docker run -v $CURDIR:$APPDIR -w $APPDIR -dit $IMAGE bash`
+
+    suite $IMAGE || {
+        echo "Tests are failed $IMAGE"
+        docker stop $RUNNER
+        exit 1
+    }
+
+    if [[ "$1" = "shell" ]]; then
+        echo "Run shell"
+        execute /bin/bash
+    fi
+
+    docker stop $RUNNER
+
+done
